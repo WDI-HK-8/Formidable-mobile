ec15200ce5132a5b91ab00b9897e513d4fd3331b
Yohan <yohanology@gmail.com>
2015-09-11 10:33:45 +0800

    (logout): Add logout in menu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,17 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $auth, $window) {
+.controller('AppCtrl', function($scope, $auth, $window, $state) {
   var validateUser = function(){
     $scope.currentUser = JSON.parse($window.localStorage.getItem('current-user'));
     console.log("current user:", $scope.currentUser)
   };
   validateUser();
+
+  $scope.logout = function(){
+    $window.localStorage.setItem('current-user', null);
+    validateUser();
+    $state.go('app.login');
+  };
 })
 
 .controller('LogInCtrl', function($scope, $auth, $window, $ionicPopup, $state) {
@@ -26,13 +32,14 @@ angular.module('starter.controllers', [])
       $window.localStorage.setItem('current-user', JSON.stringify(resp));
       validateUser();
 
-      $state.go('app.forms')
+      $state.go('app.forms');
+      $scope.loginData = {};
 
     }).catch(function(resp){
       console.log(resp);
       $ionicPopup.alert({
-        title: 'wrong',
-        template: 'try again'
+        title: 'Could not login',
+        template: 'Please try again'
       });
     });
   };
